d65917093281d5a450b9af250ea43b9503f3ee17
Mohnish18 <mohnish.chaudhary@exeloncorp.com>
2018-06-07 16:33:55 +0530

    What:quick sort 3 way partition

--- week4_divide_and_conquer/3_improving_quicksort/sorting.py
+++ week4_divide_and_conquer/3_improving_quicksort/sorting.py
@@ -3,12 +3,25 @@ import sys
 import random
 
 def partition3(a, l, r):
-    #write your code here
-    pass
+    x = a[l]
+    j = k = l
+    for i in range(l+1, r+1):
+        # print(a)
+        if a[i] < x:
+            j += 1
+            k += 1
+            a[k], a[i] = a[i], a[k]
+            a[j], a[k] = a[k], a[j]
+        elif a[i] == x:
+            k += 1
+            a[k], a[i] = a[i], a[k]
+    print(j, k)
+    a[l], a[j] = a[j], a[l]
+    return j, k
 
 def partition2(a, l, r):
     x = a[l]
-    j = l;
+    j = l
     for i in range(l + 1, r + 1):
         if a[i] <= x:
             j += 1
@@ -21,12 +34,13 @@ def randomized_quick_sort(a, l, r):
     if l >= r:
         return
     k = random.randint(l, r)
+    print(k)
     a[l], a[k] = a[k], a[l]
-    #use partition3
-    m = partition2(a, l, r)
-    randomized_quick_sort(a, l, m - 1);
-    randomized_quick_sort(a, m + 1, r);
-
+    m1, m2 = partition3(a, l, r)
+    # print(a)
+    # print(m1, m2)
+    randomized_quick_sort(a, l, m1 - 1)
+    randomized_quick_sort(a, m2 + 1, r)
 
 if __name__ == '__main__':
     input = sys.stdin.read()
