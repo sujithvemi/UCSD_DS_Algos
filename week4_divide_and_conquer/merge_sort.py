aa36bac46f96742b11fbf76f91fa743b882164a6
Mohnish18 <mohnish.chaudhary@exeloncorp.com>
2018-06-07 21:50:14 +0530

    What:num_inversions

---
+++ week4_divide_and_conquer/merge_sort.py
@@ -0,0 +1,38 @@
+import sys
+import math
+
+def merge(b, c):
+    d = []
+    count = 0
+    while b and c:
+        if b[0] <= c[0]:
+            d.append(b[0])
+            b.pop(0)
+        else:
+            d.append(c[0])
+            c.pop(0)
+            count += len(b)
+    while b:
+        d.extend(b)
+        b = []
+    while c:
+        d.extend(c)
+        c = []
+    return d, count
+
+def merge_sort(a):
+    inversions = 0
+    if len(a) == 1:
+        return a, inversions
+    mid = math.floor(len(a)/2)
+    b, inv_l = merge_sort(a[:mid])
+    c, inv_r = merge_sort(a[mid:])
+    a_sort, merge_inv = merge(b, c)
+    inversions = inv_l + inv_r + merge_inv
+    return a_sort, inversions
+
+if __name__ == "__main__":
+    input_vals = sys.stdin.read()
+    n, *a = list(map(int, input_vals.split()))
+    _, n_inv = merge_sort(a)
+    print(n_inv)
