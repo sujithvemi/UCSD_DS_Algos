688f144aa0e70d53158d81a845d61bbe671b253b
Mohnish18 <mohnish.chaudhary@exeloncorp.com>
2018-06-12 21:35:00 +0530

    What:Tree height problem

--- 2_Data_Structures/1_week/tree_height/tree-height.py
+++ 2_Data_Structures/1_week/tree_height/tree-height.py
@@ -5,25 +5,17 @@ sys.setrecursionlimit(10**7) # max depth of recursion
 threading.stack_size(2**27)  # new thread will get stack of such size
 
 class TreeHeight:
-        def read(self):
-                self.n = int(sys.stdin.readline())
-                self.parent = list(map(int, sys.stdin.readline().split()))
+	def read(self):
+		self.n = int(sys.stdin.readline())
+		self.parent = list(map(int, sys.stdin.readline().split()))
+		
 
-        def compute_height(self):
-                # Replace this code with a faster implementation
-                maxHeight = 0
-                for vertex in range(self.n):
-                        height = 0
-                        i = vertex
-                        while i != -1:
-                                height += 1
-                                i = self.parent[i]
-                        maxHeight = max(maxHeight, height);
-                return maxHeight;
+	def compute_height(self):
+        
 
 def main():
-  tree = TreeHeight()
-  tree.read()
-  print(tree.compute_height())
+	tree = TreeHeight()
+	tree.read()
+	print(tree.compute_height())
 
 threading.Thread(target=main).start()
