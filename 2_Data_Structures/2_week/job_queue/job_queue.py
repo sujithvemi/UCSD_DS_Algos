842c83330eda3731364c54d69b27e5efa935e7f1
sujithvemi <sujith.3920@gmail.com>
2018-06-20 20:18:57 +0530

    What:parallel processing simulation code

--- 2_Data_Structures/2_week/job_queue/job_queue.py
+++ 2_Data_Structures/2_week/job_queue/job_queue.py
@@ -1,28 +1,60 @@
 # python3
+import math
+
+class Thread:
+    def __init__(self, i):
+        self.id = i
+        self.next_free = 0
 
 class JobQueue:
     def read_data(self):
-        self.num_workers, m = map(int, input().split())
+        self.num_workers, self.m = map(int, input().split())
         self.jobs = list(map(int, input().split()))
-        assert m == len(self.jobs)
+        assert self.m == len(self.jobs)
 
     def write_response(self):
         for i in range(len(self.jobs)):
-          print(self.assigned_workers[i], self.start_times[i]) 
-
+            print(self.assigned_workers[i], self.start_times[i]) 
+    
+    def parent(self, i):
+        return math.floor((i-1)/2)
+    
+    def left_child(self, i):
+        return 2*i+1
+    
+    def right_child(self, i):
+        return 2*i+2
+    
+    def sift_down(self, i):
+        min_i = i
+        l = self.left_child(i)
+        if l < self.num_workers and ((self.thread_list[l].next_free < self.thread_list[min_i].next_free)\
+            or (self.thread_list[l].next_free == self.thread_list[min_i].next_free and\
+            self.thread_list[l].id < self.thread_list[min_i].id)):
+           min_i = l
+        r = self.right_child(i)
+        if r < self.num_workers and ((self.thread_list[r].next_free < self.thread_list[min_i].next_free)\
+            or (self.thread_list[r].next_free == self.thread_list[min_i].next_free\
+            and self.thread_list[r].id < self.thread_list[min_i].id)):
+            min_i = r
+        if min_i != i:
+            self.thread_list[i], self.thread_list[min_i] = self.thread_list[min_i], self.thread_list[i]
+            self.sift_down(min_i)
+        else:
+            return
+    
     def assign_jobs(self):
-        # TODO: replace this code with a faster algorithm.
-        self.assigned_workers = [None] * len(self.jobs)
-        self.start_times = [None] * len(self.jobs)
-        next_free_time = [0] * self.num_workers
-        for i in range(len(self.jobs)):
-          next_worker = 0
-          for j in range(self.num_workers):
-            if next_free_time[j] < next_free_time[next_worker]:
-              next_worker = j
-          self.assigned_workers[i] = next_worker
-          self.start_times[i] = next_free_time[next_worker]
-          next_free_time[next_worker] += self.jobs[i]
+        self.thread_list = []
+        self.assigned_workers = []
+        self.start_times = []
+        for i in range(self.num_workers):
+            self.thread_list.append(Thread(i))
+        for job_no in range(self.m):
+            next_worker = self.thread_list[0]
+            self.assigned_workers.append(next_worker.id)
+            self.start_times.append(next_worker.next_free)
+            next_worker.next_free += self.jobs[job_no]
+            self.sift_down(0)
 
     def solve(self):
         self.read_data()
