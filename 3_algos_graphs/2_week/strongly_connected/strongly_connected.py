0bfff238580e6a5a5282e40b9a476a43c852955b
sujithvemi <sujith.3920@gmail.com>
2018-07-10 22:39:04 +0530

    What: Strongly connected components in a directed graph

--- 3_algos_graphs/2_week/strongly_connected/strongly_connected.py
+++ 3_algos_graphs/2_week/strongly_connected/strongly_connected.py
@@ -4,10 +4,41 @@ import sys
 
 sys.setrecursionlimit(200000)
 
+def Explore(adj, i, visited, *args):
+    visited[i] = True
+    for j in adj[i]:
+        if visited[j] == True:
+            continue
+        else:
+            if args:
+                post_order = args[0]
+                if post_order is not None:
+                    Explore(adj, j, visited, post_order)
+            else:
+                Explore(adj, j, visited)
+    if args:
+        post_order = args[0]
+        if post_order is not None:
+            post_order.append(i)
+
+def dfs(adj):
+    visited = [False for _ in range(n)]
+    post_order = []
+    Explore(adj, 0, visited, post_order)
+    return post_order
 
 def number_of_strongly_connected_components(adj):
     result = 0
-    #write your code here
+    visited = [False for _ in range(n)]
+    rev_adj = [[] for _ in range(n)]
+    for i in range(n):
+        for target in adj[i]:
+            rev_adj[target].append(i)
+    post_order = dfs(rev_adj)
+    for v in reversed(post_order):
+        if not visited[v]:
+            result += 1
+            Explore(adj, v, visited)
     return result
 
 if __name__ == '__main__':
